ec1229a5ad84f6db56ca1c7a6c91e3c84d676177
Abhi Basu Thakur <abhibt@yahoo.com>
2021-05-01 21:34:19 +0530

    Update put-item.js

--- src/handlers/put-item.js
+++ src/handlers/put-item.js
@@ -19,18 +19,18 @@ exports.putItemHandler = async (event) => {
     // All log statements are written to CloudWatch by default. For more information, see
     // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
     console.log('received:', JSON.stringify(event));
-/*
+
     //Get id and name from the body of the request
-    const { id, name } = JSON.parse(body);
+    const { id, eId } = JSON.parse(body);
 
     // Creates a new item, or replaces an old item with a new item
     // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
     const params = {
         TableName: tableName,
-        Item: { id, name },
-    };*/
+        Item: { id, eId },
+    };
     
-  
+  /*
     // Get id and name from the body of the request
     const { id,name,apartmentName, userName,phoneNumber,eId } = JSON.parse(body);
 
@@ -40,7 +40,7 @@ exports.putItemHandler = async (event) => {
         TableName: tableName,
         Item: { id,name,apartmentName, userName,phoneNumber,eId }
     };
-   
+   */
     await docClient.put(params).promise();
 
     const response = {
